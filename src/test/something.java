7b56853a4219b3f85045e86b2c14d16952102b56
Lego <kushdalal007@gmail.com>
2019-01-09 09:06:26 -0500

    Some minor changes

--- src/test/something.java
+++ src/test/something.java
@@ -5,6 +5,14 @@ public class something {
 	public static void main(String Args[]) 
 	{
 		System.out.println("Hello World");
+		Loop();
+	}
+	
+	public static void Loop() {
+		System.out.println("Hi");
+	}
+	public void test() {
+		System.out.println("hi again");
 	}
 	
 
