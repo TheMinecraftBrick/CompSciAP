26dc1da8eb83ea97071a9760b47cd84ccb8dc403
LegoDude <kushdalal007@gmail.com>
2019-01-14 13:05:57 -0500

    Started lab 8

---
+++ src/labs/Palindrome.java
@@ -0,0 +1,38 @@
+package labs;
+
+public class Palindrome {
+	
+
+		public static boolean isPal(String s)
+		{
+	     int n = s.length();
+	     for  (int k = 0; k < n; k++)
+	    	 System.out.println(s.substring(k));      
+	     System.out.println();
+			
+			
+	      return true; 
+		}  
+	   
+
+	   private static boolean isLetter(String letter)
+	   {
+	      return true;      
+	      
+	   }
+	   
+		
+		private static String purge(String s)
+		{
+	      return "";       
+	      
+		}
+	   
+	   
+		public static boolean almostPal(String s)
+		{
+	      return true;      
+		}
+
+	
+}
--- src/test/something.java
+++ src/test/something.java
@@ -5,17 +5,7 @@ public class something {
 	public static void main(String Args[]) 
 	{
 		System.out.println("Hello World");
-		Loop();
 	}
 	
-	public static void Loop() {
-		System.out.println("Hi");
-	}
-	public void test() {
-		System.out.println("hi again");
-	}
-	public float bob() {
-		return true;
-		
-	}
+	
 }
