ce708cb5f4de0d6148590cab124498d7b377869a
LegoDude <kushdalal007@gmail.com>
2019-02-15 15:02:15 -0500

    lab 11

--- src/lab11/Lab11avst.java
+++ src/lab11/Lab11avst.java
@@ -8,34 +8,58 @@ package lab11;
 
 import java.util.Scanner;
 
-
 public class Lab11avst
 {
 	public static void main(String[] args)
 	{
 		// This main method needs additions for the 100 point version.
 		Scanner input = new Scanner(System.in);
-		final int MAX = 100;
-		boolean primes[];
-		primes = new boolean[MAX];
+		System.out.print("Enter the primes upper bound  ===>>  ");
+		final int MAX = input.nextInt();
+		boolean primes[] = new boolean[MAX];
 		computePrimes(primes);
 		displayPrimes(primes);
 	}
 
 	public static void computePrimes(boolean primes[])
 	{
+		for (int i = 0; i < primes.length; i++)
+		{
+			primes[i] = true;
+		}
+
+		for (int i = 0; i < primes.length; i++)
+		{
+			if (primes[i])
+			{
+				primes[i] = isPrime(i);
+			}
+		}
 		System.out.println("\nCOMPUTING PRIME NUMBERS");
 
 	}
 
 	public static void displayPrimes(boolean primes[])
 	{
-		System.out.println("\n\nPRIMES BETWEEN 1 AND "+ primes.length);
-		System.out.println();
+		System.out.println("\n\nPRIMES BETWEEN 1 AND " + primes.length);
+		System.out.println(primes);
+
+	}
+
+	public static boolean isPrime(int num)
+	{
+		for (int i = 2; i <= num / 2; i++)
+		{
+			if (num % i == 0)
+			{
+				return false;
+			}
+		}
+		if (num <= 1)
+			return false; 
+		else
+			return true;
 
 	}
 
 }
-
-
-
