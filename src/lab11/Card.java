21040ec0307de5d5a93eced206f619b83de042ec
LegoDude <kushdalal007@gmail.com>
2019-03-28 12:17:26 -0400

    updates to lab 11

--- src/lab11/Card.java
+++ src/lab11/Card.java
@@ -1,5 +1,6 @@
 package lab11;
 // 3/5/19 Kush Dalal
+
 // 
 // Card.java
 // This Card class is identical to the Card03 class.
@@ -9,38 +10,58 @@ package lab11;
 // by Michael Clancy, Robert Glen Martin and Judith Hromcik.
 // Divided into stages and altered August 2014 by Leon Schram.
 
-
 public class Card
 {
-   private String suit;
-   private String rank;
-   private int value;
+	private String suit;
+	private String rank;
+	private int value;
 
-   public Card(String s, String r, int v)
-   {
-      suit = s;
-      rank = r;
-      value = v;
-   }
+	public Card(String s, String r, int v)
+	{
+		suit = s;
+		rank = r;
+		value = v;
+	}	
+	
+	
+	public String getSuit()
+	{
+		return suit;
+	}
 
-   public String getSuit()       { return suit; }
-   public String getRank()       { return rank; }
-   public int getValue()         { return value; }
-   
-   public void setSuit(String s) { suit = s; }
-   public void setRank(String r) { rank = r; }
-   public void setValue(int v)   { value = v; } 
-   
-   public String toString()
-   {
-      return "[" + suit + ", " + rank + ", " + value + "]";
-   }
-   
-   public boolean matches(Card otherCard) 
-   {
-		return otherCard.getSuit().equals(this.suit)
-			 && otherCard.getRank().equals(this.rank)
-			 && otherCard.getValue() == this.value;
+	public String getRank()
+	{
+		return rank;
+	}
+
+	public int getValue()
+	{
+		return value;
+	}
+
+	public void setSuit(String s)
+	{
+		suit = s;
+	}
+
+	public void setRank(String r)
+	{
+		rank = r;
+	}
+
+	public void setValue(int v)
+	{
+		value = v;
+	}
+
+	public String toString()
+	{
+		return "[" + suit + ", " + rank + ", " + value + "]";
+	}
+
+	public boolean matches(Card otherCard)
+	{
+		return otherCard.getSuit().equals(this.suit) && otherCard.getRank().equals(this.rank)
+				&& otherCard.getValue() == this.value;
 	}
 }
-
